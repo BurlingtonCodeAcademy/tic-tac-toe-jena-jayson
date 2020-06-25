8cdd30bfff9e9e7479548eccb7d4d5758d3d1b9a
JenaDurell <66094197+JenaDurell@users.noreply.github.com>
2020-06-25 16:48:03 -0400

    thurs445pm

--- index.js
+++ index.js
@@ -1,68 +1,52 @@
+let startButton = document.getElementById('start-button');
+let statusBox = document.getElementById('status')
+let allBoxes = document.getElementsByClassName('cell')
 let gameArray = ['', '', '', '', '', '', '', '', '']
-let startButton = document.getElementById('start');
-let status = document.getElementById('status');
-let cells = Array.from(document.getElementsByClassName('cell'));
-let playerOne = document.getElementById('playerOne');
-let playerTwo = document.getElementsById('playerTwo');
+let player = 'X'
+
+function startGame() {
+    startButton.addEventListener('click', (event) => {
+        startButton.classList.add('start-button-active');
+        statusBox.innerHTML = 'Player X\'s turn';
+        startButton.disabled = true
+    })
+    gameArray.forEach(async function (box, i) {
+        let boxID = 'box' + (i + 1);
+        let currentBox = document.getElementById(boxID);
+        currentBox.addEventListener('click', (event) => {
+            gameArray[currentBox.id[3]-1]=player
+            drawBoard(gameArray)
+            if (player === 'X') {
+                player = 'O'
+              } else {
+                player = 'X'
+              }
+              statusBox.textContent = `Player ${player}'s turn`
+            
+            
 
 
-let wins = [1, 2, 3][4, 5, 6][7, 8, 9]
-[1, 4, 7][2, 5, 8][3, 6, 9]
-[1, 5, 9][3, 5, 7]
-
-
-
-function start() {
-  startButton.addEventListener('click', (event) => {
-    alert('Goodbye')
-    myButton.disabled = true
-  })
-
-  
-  })
-  playerOne = (playerOne.value === '' ? 'X' : playerOne.value);
-  playerTwo = (playerTwo.value === '' ? 'O' : playerTwo.value);
-  status.textContent = `Player ${player}'s turn`;
-  };
-
-function clicked(event) {
-  event.target.textContent = player;
-  event.target.removeEventListner('click', clicked);
-  event.target.addEventListener('click', alreadyClicked);
-  boardState[parseInt(event.target.id)] = player
-
-  checkWin(player);
-
-
-  if (player === 'x') {
-      player = 'o'
-
-  } else {
-      player = 'x'
-  }
-
-  status.textContent = `Player ${player}'s turn`
-}
-
-
-function alreadyClicked() {
-  status.textContent = `already clicked`
-
-
-drawBoard(gameArray)
-function drawBoard(currentBoard) {
-    currentBoard.forEach(async function (box, i) {
-      let boxID = 'box' + (i + 1) ;
-      console.log(boxID) ;
-      let currentBox = document.getElementById(boxID);
-      currentBox.innerHTML = box;
+        } )
     })
 
+
+
+
 }
 
-let start = document.getElementById("start")
-let status = document.getElementById("status")
+// let startButton = document.getElementsByTagName('div')[0]
+// myDiv.addEventListener('click', () => {
+//     event.target.style.backgroundColor = "red"
+// })
 
-start.addEventListener("click", (event) => {
-  event.disable
-})
+
+
+function drawBoard(currentBoard) {
+    currentBoard.forEach(async function (box, i) {
+        let boxID = 'box' + (i + 1);
+        let currentBox = document.getElementById(boxID);
+        currentBox.innerHTML = box;
+    })
+}
+
+startGame()
---
+++ unused.js
@@ -0,0 +1,65 @@
+let gameArray = ['', '', '', '', '', '', '', '', '']
+let startButton = document.getElementById('start');
+let status = document.getElementById('status');
+let cells = Array.from(document.getElementsByClassName('cell'));
+
+let playerOne = document.getElementById('playerOne');
+let playerTwo = document.getElementById('playerTwo');
+
+
+let wins = [[1, 2, 3],[4, 5, 6], [7, 8, 9],
+[1, 4, 7], [2, 5, 8], [3, 6, 9],
+[1, 5, 9], [3, 5, 7]]
+
+
+
+// function startGame() {
+//   startButton.addEventListener('click', (event) => {
+//     alert('Goodbye')
+//     myButton.disabled = true
+//   })
+// }
+
+
+playerOne = (playerOne.value === '' ? 'X' : playerOne.value);
+playerTwo = (playerTwo.value === '' ? 'O' : playerTwo.value);
+let player = playerOne
+
+status.textContent = `Player ${player}'s turn`;
+
+
+function clicked(event) {
+  event.target.textContent = player;
+  event.target.removeEventListner('click', clicked);
+  event.target.addEventListener('click', alreadyClicked);
+  boardState[parseInt(event.target.id)] = player
+
+  if (player === 'x') {
+    player = 'o'
+  } else {
+    player = 'x'
+  }
+  status.textContent = `Player ${player}'s turn`
+}
+
+
+function alreadyClicked() {
+  status.textContent = `already clicked`
+}
+
+drawBoard(gameArray)
+function drawBoard(currentBoard) {
+  currentBoard.forEach(async function (box, i) {
+    let boxID = 'box' + (i + 1);
+    console.log(boxID);
+    let currentBox = document.getElementById(boxID);
+    currentBox.innerHTML = box;
+    })
+}
+
+// let start = document.getElementById("start")
+// let status = document.getElementById("status")
+
+
+
+startGame()
